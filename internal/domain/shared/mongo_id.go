f3f68786e5f24443344430d11a5afa5e659caac7
cassius <cassiusbessa@gmail.com>
2025-05-06 02:14:12 -0300

    feat: :sparkles: add comments to mongo ID utility functions for better clarity

--- internal/domain/shared/mongo_id.go
+++ internal/domain/shared/mongo_id.go
@@ -6,10 +6,12 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// GenerateMongoID gera um novo ObjectID aleatório para ser usado como ID
 func NewMongoID() primitive.ObjectID {
 	return primitive.NewObjectID()
 }
 
+// ObjectIDFromString converte uma string para um ObjectID
 func ObjectIDFromString(s string) (primitive.ObjectID, error) {
 	objectID, err := primitive.ObjectIDFromHex(s)
 	if err != nil {
@@ -19,6 +21,7 @@ func ObjectIDFromString(s string) (primitive.ObjectID, error) {
 	return objectID, nil
 }
 
+// ObjectIDToString converte um ObjectID para uma string, se o ObjectID for vazio, retorna uma string vazia
 func ObjectIDToString(o primitive.ObjectID) string {
 	return o.Hex()
 }
