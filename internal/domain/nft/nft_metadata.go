299449dad8593df7feaed192e723e7a9e9b2a1c8
cassius <cassiusbessa@gmail.com>
2025-05-13 02:30:38 -0300

    fix: :bug: update bson tag for ID field in NFT struct and add Description field; refactor NewNFTMetadata function

--- internal/domain/nft/nft_metadata.go
+++ internal/domain/nft/nft_metadata.go
@@ -5,14 +5,12 @@ import (
 )
 
 type NFTMetadata struct {
-	Title  string     `json:"title" bson:"title"`
 	Image  string     `json:"image" bson:"image"`
 	Expiry *time.Time `json:"expiry" bson:"expiry"`
 }
 
-func NewNFTMetadata(title, image string, expiry *time.Time) NFTMetadata {
-	return NFTMetadata{
-		Title:  title,
+func NewNFTMetadata(image string, expiry *time.Time) *NFTMetadata {
+	return &NFTMetadata{
 		Image:  image,
 		Expiry: expiry,
 	}
