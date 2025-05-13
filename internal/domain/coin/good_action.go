080875be8a619e33545f2fdd2b04a5c41ec39bd7
cassius <cassiusbessa@gmail.com>
2025-05-13 01:39:13 -0300

    fix: :bug: correct bson tag for ID field in GoodAction struct

--- internal/domain/coin/good_action.go
+++ internal/domain/coin/good_action.go
@@ -6,7 +6,7 @@ import (
 )
 
 type GoodAction struct {
-	ID              primitive.ObjectID `json:"id" bson:"id"`
+	ID              primitive.ObjectID `json:"id" bson:"_id"`
 	Name            string             `json:"name" bson:"name"`
 	Description     string             `json:"description" bson:"description"`
 	RewardAmount    uint64             `json:"reward_amount" bson:"reward_amount"`
