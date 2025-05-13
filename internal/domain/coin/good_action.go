8d865a81c5ba09c6798ade6a72a2e26789afe40e
cassius <cassiusbessa@gmail.com>
2025-05-13 02:30:52 -0300

    feat: :sparkles: add GivenNFT field to GoodAction struct and update NewGoodAction function

--- internal/domain/coin/good_action.go
+++ internal/domain/coin/good_action.go
@@ -10,16 +10,18 @@ type GoodAction struct {
 	Name            string             `json:"name" bson:"name"`
 	Description     string             `json:"description" bson:"description"`
 	RewardAmount    uint64             `json:"reward_amount" bson:"reward_amount"`
+	GivenNFT        *nft.NFT           `json:"given_nft" bson:"given_nft"`
 	AllowedNFTTypes []nft.NFT          `json:"allowed_nft_types" bson:"allowed_nft_types"`
 	BlockedNFTTypes []nft.NFT          `json:"blocked_nft_types" bson:"blocked_nft_types"`
 }
 
-func NewGoodAction(name, description string, rewardAmount uint64, allowedNFTTypes, blockedNFTTypes []nft.NFT) *GoodAction {
+func NewGoodAction(name, description string, rewardAmount uint64, givenNFT *nft.NFT, allowedNFTTypes, blockedNFTTypes []nft.NFT) *GoodAction {
 	return &GoodAction{
 		ID:              primitive.NewObjectID(),
 		Name:            name,
 		Description:     description,
 		RewardAmount:    rewardAmount,
+		GivenNFT:        givenNFT,
 		AllowedNFTTypes: allowedNFTTypes,
 		BlockedNFTTypes: blockedNFTTypes,
 	}
