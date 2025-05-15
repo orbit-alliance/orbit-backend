fd3846af4bb3e4d0a1e6ed6ece5df3468e9d8763
cassius <cassiusbessa@gmail.com>
2025-05-15 02:57:22 -0300

    feat: :sparkles: integrate event bus and on-chain publisher for DidGoodAction events

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,11 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/orbit-alliance/orbit-backend/internal/application/user/services"
+	"github.com/orbit-alliance/orbit-backend/internal/domain/shared"
+	"github.com/orbit-alliance/orbit-backend/internal/domain/user"
 	"github.com/orbit-alliance/orbit-backend/internal/infra/db"
+	"github.com/orbit-alliance/orbit-backend/internal/infra/web3"
 )
 
 func main() {
@@ -22,6 +26,14 @@ func main() {
 
 	db.InitMongoDB()
 	defer db.CloseMongoDB()
+
+	eventBus := shared.NewEventBus()
+	ethGateway, err := web3.NewEthGateway(64)
+	if err != nil {
+		log.Fatal("Erro ao criar o EthGateway", err)
+	}
+	onChainPublisher := services.NewOnChainPublisher(*ethGateway)
+	eventBus.Subscribe(user.DidGoodAction{}.EventType(), onChainPublisher.Handler)
 
 	goodActionRepo := db.NewGoodActionRepository(db.Database)
 	db.SeedGoodActions(context.TODO(), goodActionRepo)
