f08e5cbcf0a6b905549b2bd9ab0c8e980762c3db
Cássius Bessa <cassiusbessa@gmail.com>
2025-05-23 04:46:47 -0300

    refactor: normalize error messages for consistency in casing

--- internal/infra/gateway/errors.go
+++ internal/infra/gateway/errors.go
@@ -3,8 +3,8 @@ package gateway_42
 import "errors"
 
 var (
-	ErrSecretsIsNotDefined     = errors.New("Client ID or Client Secret is not defined")
-	ErrNoAccessTokenInResponse = errors.New("No access token found in response")
+	ErrSecretsIsNotDefined     = errors.New("client ID or Client Secret is not defined")
+	ErrNoAccessTokenInResponse = errors.New("no access token found in response")
 	ErrFailToCreate42Token     = errors.New("user: fail to create 42 token")
 	ErrFailToGetLocationIn42   = errors.New("user: fail to get location in 42")
 	ErrFailToGetUserIn42       = errors.New("user: fail to get user in 42")
