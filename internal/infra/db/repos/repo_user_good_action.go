1f653973e208c118c2a647920c0815ffc46827f5
Cássius Bessa <cassiusbessa@gmail.com>
2025-05-23 05:25:17 -0300

    feat: implement UserGoodActionRepository with Save method for user actions

---
+++ internal/infra/db/repos/repo_user_good_action.go
@@ -0,0 +1,36 @@
+package repo
+
+import (
+	"context"
+
+	"github.com/orbit-alliance/orbit-backend/internal/domain/user"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type UserGoodActionRepository struct {
+	collection *mongo.Collection
+}
+
+func NewUserGoodActionRepository(db *mongo.Database) *UserGoodActionRepository {
+	collection := db.Collection("NFts")
+	return &UserGoodActionRepository{collection: collection}
+}
+
+func (r *UserGoodActionRepository) Save(ctx context.Context, userGoodAction *user.UserGoodAction) error {
+
+	filter := bson.M{"_id": userGoodAction.ID}
+	update := bson.M{
+		"$set": userGoodAction,
+	}
+
+	opts := options.Update().SetUpsert(true)
+
+	_, err := r.collection.UpdateOne(ctx, filter, update, opts)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
