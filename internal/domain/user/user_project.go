23c87fc0b05235f5bfb672646903ab04245806e7
Cássius Bessa <cassiusbessa@gmail.com>
2025-05-23 04:49:02 -0300

    feat: :sparkles: implement do bonus project method and change doGoodAction to private method

---
+++ internal/domain/user/user_project.go
@@ -0,0 +1,21 @@
+package user
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type UserProject struct {
+	ID       primitive.ObjectID `json:"id" bson:"_id"`
+	Name     string             `json:"name" bson:"name"`
+	MaxScore uint8              `json:"max_score" bson:"max_score"`
+	UserID   primitive.ObjectID `json:"user_id" bson:"user_id"`
+	UserName string             `json:"user_name" bson:"user_name"`
+}
+
+func NewUserProject(id primitive.ObjectID, name string, maxScore uint8, userID primitive.ObjectID, userName string) *UserProject {
+	return &UserProject{
+		ID:       id,
+		Name:     name,
+		MaxScore: maxScore,
+		UserID:   userID,
+		UserName: userName,
+	}
+}
